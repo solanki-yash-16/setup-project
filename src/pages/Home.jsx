0db71d07a674cfd45f8b89682efaaccf98e71737
solanki-yash-16 <yashsolanki0777@gmail.com>
2025-07-02 23:05:31 +0530

    Add the contact form and also integrate the basic api and route setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { getAllUser } from "../api/users";
+import React, { useEffect } from "react";
 
 const Home = () => {
-  return (
-    <div>Home Page</div>
-  )
-}
+  const getUserData = async () => {
+    try {
+      const response = await getAllUser();
+      if (response) {
+        console.log("res", response);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getUserData();
+  }, []);
+  return <div>Home Page</div>;
+};
 
 export default Home;
